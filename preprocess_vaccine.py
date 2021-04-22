4f209b8a4e74a97569b3ec5eb10e325076f1abe8
giovanniardenghi <36991376+giovanniardenghi@users.noreply.github.com>
2021-04-22 09:55:28 +0200

    Update preprocess_vaccine.py

--- preprocess_vaccine.py
+++ preprocess_vaccine.py
@@ -4,15 +4,16 @@ import numpy as np
 data = pd.read_csv('https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-latest.csv')
 
 data['data_somministrazione'] = pd.to_datetime(data['data_somministrazione'])
-data = data.groupby('data_somministrazione').sum()
-
-repeated_values = data.iloc[-7:,1:]
-first_forecast = max(data.index) + pd.Timedelta(1,'day')
-last_repeated = max(data.index) + pd.Timedelta(22,'day') 
-new_max = max(data.index) + pd.Timedelta(35,'days')
+data_Italia = data.groupby('data_somministrazione').sum()
+ 
+new_max = max(data_Italia.index) + pd.Timedelta(35,'days')
 new_index = pd.date_range('2020-02-24',new_max)
 
-data = data.reindex(new_index,columns=['prima_dose','seconda_dose']).ffill()
+data_Italia = data_Italia.reindex(new_index,columns=['prima_dose','seconda_dose']).ffill()
 
-
-data.to_csv('data/vaccines.csv',index_label='data')
+regions = [x for _, x in data.groupby(['data_somministrazione','area'])]
+for x in regions:
+    data_reg = x.reindex(new_index,columns=['prima_dose','seconda_dose']).ffill()
+    x.to_csv('data/vaccini_regioni/'+x.area[0]+'.csv',index=None)
+    
+data_Italia.to_csv('data/vaccines.csv',index_label='data')
