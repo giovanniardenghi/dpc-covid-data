70a8c5cab1cef35d5961da8e8373c3ec3dd80a90
Giovanni Ardenghi <giovanni.ardenghi@polimi.it>
2021-11-24 12:54:48 +0100

    added green pass from test

---
+++ preprocess_green_pass.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+gp = pd.read_csv('https://raw.githubusercontent.com/ministero-salute/it-dgc-opendata/master/data/dgc-issued.csv')
+
+gp['data'] = pd.to_datetime(gp.data)
+gp.set_index('data', inplace=True)
+gp.sort_index(inplace=True)
+gp.loc[pd.to_datetime('2020-02-24')] = 0
+gp.loc[gp.index.max() + pd.Timedelta(1, 'day')] = gp.iloc[-7:, :].mean().round()
+gp = gp.reindex(pd.date_range('2020-02-24', gp.index.max()+pd.Timedelta(100,'days'))).ffill()
+gp.issued_from_test.to_csv('data/gp_from_test.csv',index_label='data')
