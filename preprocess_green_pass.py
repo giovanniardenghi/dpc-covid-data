4370da58ee45e9441c565183c037508794cb5d22
Giovanni Ardenghi <giovanni.ardenghi@polimi.it>
2021-11-24 12:59:33 +0100

    bug fixed in preprocess_green_pass

--- preprocess_green_pass.py
+++ preprocess_green_pass.py
@@ -8,4 +8,4 @@ gp.sort_index(inplace=True)
 gp.loc[pd.to_datetime('2020-02-24')] = 0
 gp.loc[gp.index.max() + pd.Timedelta(1, 'day')] = gp.iloc[-7:, :].mean().round()
 gp = gp.reindex(pd.date_range('2020-02-24', gp.index.max()+pd.Timedelta(100,'days'))).ffill()
-gp.issued_from_test.to_csv('data/gp_from_test.csv',index_label='data')
+gp.issued_for_tests.to_csv('data/gp_from_test.csv',index_label='data')
