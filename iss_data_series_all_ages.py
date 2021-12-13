ee1726a81bc11d8c5bb3bcb7f357f00a0d8eca93
Giovanni Ardenghi <giovanni.ardenghi@polimi.it>
2021-12-13 10:27:00 +0100

    added vaccines by age

---
+++ iss_data_series_all_ages.py
@@ -0,0 +1,96 @@
+## Import modules
+import pandas as pd
+import functools
+import numpy as np
+
+## Define variables
+ignore_sex = True
+sub = 0  # value replacing '<5'
+save_csv = True
+save_txt = True
+age_groups = {'0-9': '0-19',
+              '10-19': '0-19',
+              '20-29': '20-39',
+              '30-39': '20-39',
+              '40-49': '40-59',
+              '50-59': '40-59',
+              '60-69': '60-79',
+              '70-79': '60-79',
+              '80-89': '80-89',
+              '>90': '90+'}
+Ages = age_groups.keys()
+age_groups = 'Età'
+state_group = {'Asintomatico':'Isolated',
+               'Pauci-sintomatico': 'Isolated',
+               'Lieve': 'Isolated',
+               'Severo':'Hospitalized',
+               'Critico':'Threatened'
+               }
+state_group = {'Asintomatico':'Infected',
+               'Pauci-sintomatico': 'Infected',
+               'Lieve': 'Infected',
+               'Severo':'Infected',
+               'Critico':'Infected'
+               }
+
+dates = pd.date_range(start='2020-12-08',end=np.datetime64('today','D'))
+sesso_eta = pd.DataFrame(columns = ['iss_date','SESSO','AGE_GROUP','DECEDUTI','CASI_CUMULATIVI'])
+stato_clinico = pd.DataFrame(columns = ['iss_date','SESSO','AGE_GROUP','STATO_CLINICO','CASI'])
+
+for i in dates:
+    try:
+        tmp=pd.read_csv('https://raw.githubusercontent.com/floatingpurr/covid-19_sorveglianza_integrata_italia/main/data/'+i.strftime('%Y-%m-%d')+'/sesso_eta.csv')
+        if tmp['iss_date'].values[0] not in sesso_eta['iss_date'].values:
+            sesso_eta = sesso_eta.append(tmp)
+        tmp=pd.read_csv('https://raw.githubusercontent.com/floatingpurr/covid-19_sorveglianza_integrata_italia/main/data/'+i.strftime('%Y-%m-%d')+'/stato_clinico.csv')
+        if tmp['iss_date'].values[0] not in stato_clinico['iss_date'].values:
+            stato_clinico = stato_clinico.append(tmp)
+    except OSError:
+        pass
+sesso_eta['iss_date'] = pd.to_datetime(sesso_eta['iss_date'], dayfirst=True)
+stato_clinico['iss_date'] = pd.to_datetime(stato_clinico['iss_date'], dayfirst=True)
+
+sesso_eta.rename(columns=dict(zip(sesso_eta.columns, ['Data', 'Sesso', 'Età', 'Deceduti', 'Casi'])), inplace=True)
+sesso_eta.replace('<5', sub, inplace=True)
+sesso_eta['Deceduti'] = pd.to_numeric(sesso_eta['Deceduti'])
+sesso_eta['Casi'] = pd.to_numeric(sesso_eta['Casi'])
+sesso_eta.set_index(['Data','Età', 'Sesso'], inplace=True)
+sesso_eta.drop('Non noto', level=2, inplace=True)
+sesso_eta.drop('Non noto', level=1, inplace=True)
+#sesso_eta.drop('Casi',axis=1,inplace=True)
+
+stato_clinico.rename(columns=dict(zip(stato_clinico.columns, ['Data', 'Sesso', 'Età', 'Stato', 'Casi'])), inplace=True)
+stato_clinico.replace('<5', sub, inplace=True)
+stato_clinico['Casi'] = pd.to_numeric(stato_clinico['Casi'])
+stato_clinico['Stato'] = stato_clinico.Stato.str.capitalize()
+stato_clinico.set_index(['Data','Età', 'Stato', 'Sesso'], inplace=True)
+stato_clinico.drop('Non noto', level=1, inplace=True)
+
+if ignore_sex:
+    sesso_eta = sesso_eta.groupby(['Data',age_groups], level=[0,1]).sum()
+    sesso_eta['Nuovi_Deceduti'] = sesso_eta.groupby(['Età'])['Deceduti'].diff()
+    stato_clinico = stato_clinico.groupby([state_group, 'Data', age_groups], level=[2, 0, 1]).sum()
+    sesso_eta['Casi'] = sesso_eta['Casi'] - sesso_eta['Deceduti'] - stato_clinico.xs('Infected', level='Stato').values.flatten()
+else:
+    sesso_eta = sesso_eta.groupby(['Data', age_groups, 'Sesso'], level=[0, 1, 2]).sum()
+    stato_clinico = stato_clinico.groupby(['Data', state_group, age_groups, 'Sesso'], level=[0, 2, 1, 3]).sum()
+
+sesso_eta_perc = sesso_eta / sesso_eta.groupby('Data').sum()
+stato_clinico_perc = stato_clinico / stato_clinico.groupby(['Stato','Data'],level=[0,1]).sum()
+
+tmp = pd.concat({'Extinct': sesso_eta_perc['Deceduti']}, names=['Stato']).to_frame(name='Casi')
+tmp3 = pd.concat({'Daily_extinct': sesso_eta_perc['Nuovi_Deceduti']}, names=['Stato']).to_frame(name='Casi')
+tmp2 = pd.concat({'Recovered': sesso_eta_perc['Casi']}, names=['Stato']).to_frame(name='Casi')
+stato_clinico_perc = stato_clinico_perc.append(tmp)
+stato_clinico_perc = stato_clinico_perc.append(tmp2)
+stato_clinico_perc = stato_clinico_perc.append(tmp3)
+
+
+stato_clinico_perc = stato_clinico_perc.reset_index().pivot(index=['Data','Età'],columns='Stato',values='Casi')
+new_index = pd.MultiIndex.from_product([pd.date_range(stato_clinico_perc.reset_index()['Data'].min(),stato_clinico_perc.reset_index()['Data'].max()),
+    Ages], names=['Data','Età'] )
+stato_clinico_perc = stato_clinico_perc.reindex(new_index)
+stato_clinico_perc = stato_clinico_perc.unstack(1).ffill().stack()
+
+#stato_clinico_perc.reset_index().drop('Età',axis=1).to_csv('stato_clinico.csv',index=None)
+stato_clinico_perc.reset_index().to_csv('SUIHTER/stato_clinico_all_ages.csv',index=None)
---
+++ preprocess_vaccine_age.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import numpy as np
+sigla_regioni = {
+    'ITA':'Italia',
+    'ABR':'Abruzzo',
+    'BAS':'Basilicata',
+    'CAL':'Calabria',
+    'CAM':'Campania',
+    'EMR':'Emilia-Romagna',
+    'FVG':'Friuli Venezia Giulia',
+    'LAZ':'Lazio',
+    'LIG':'Liguria',
+    'LOM':'Lombardia',
+    'MAR':'Marche',
+    'MOL':'Molise',
+    'PAB':'P.A. Bolzano',
+    'PAT':'P.A. Trento',
+    'PIE':'Piemonte',
+    'PUG':'Puglia',
+    'SAR':'Sardegna',
+    'SIC':'Sicilia',
+    'TOS':'Toscana',
+    'UMB':'Umbria',
+    'VDA':'Valle d\'Aosta',
+    'VEN':'Veneto'
+}
+
+pops = pd.read_csv('https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/platea.csv')
+pops = pops.groupby('area').sum().totale_popolazione
+pops['ITA'] = pops.sum()
+
+data = pd.read_csv(
+    'https://raw.githubusercontent.com/italia/covid19-opendata-vaccini/master/dati/somministrazioni-vaccini-latest.csv')
+
+data['data_somministrazione'] = pd.to_datetime(data['data_somministrazione'])
+#data = data[data['data_somministrazione']<='2021-03-25']
+monodose = data[data['fornitore']=='Janssen']
+
+#data.drop(monodose.index, inplace=True)
+monodose = monodose.groupby(['data_somministrazione','fascia_anagrafica']).sum()
+data_Italia =  data.groupby(['data_somministrazione','fascia_anagrafica']).sum()
+data_Italia['mono_dose'] = monodose['prima_dose']
+data_Italia.fillna(0,inplace=True)
+
+max_Italia_index = max(data_Italia.index)[0]
+new_max = max_Italia_index# + pd.Timedelta(100, 'days')
+new_index = pd.date_range(min(data_Italia.index)[0], new_max)
+new_index = pd.MultiIndex.from_product([new_index, ('12-19','20-29','30-39','40-49','50-59','60-69','70-79','80-89','90+')])
+
+today = str(pd.Timestamp.today()+pd.Timedelta(2, 'day'))[:10]
+#today = '2021-03-26'
+print(today)
+
+data_Italia['prima_dose'] = data_Italia.prima_dose-data_Italia.mono_dose
+data_Italia['seconda_dose'] = data_Italia.seconda_dose+data_Italia.mono_dose+data_Italia.pregressa_infezione
+data_Italia['terza_dose'] = data_Italia.dose_addizionale_booster
+
+print(data_Italia)
+#data_Italia.loc[pd.to_datetime(today),['prima_dose','seconda_dose']] = 3e5
+#data_Italia.loc[pd.to_datetime(today),['terza_dose']] = 4e5
+#data_Italia.loc[max_Italia_index + pd.Timedelta(1, 'day')] = data_Italia.iloc[-7:, :].mean().round()
+data_Italia = data_Italia.reindex(new_index, columns=['prima_dose', 'seconda_dose', 'terza_dose', 'pregressa_infezione', 'mono_dose'], fill_value=0)
+#data_Italia['prima_dose_tot'] = data_Italia.prima_dose.cumsum()
+#data_Italia['seconda_dose_tot'] = data_Italia.seconda_dose.cumsum()
+#data_Italia['terza_dose_tot'] = data_Italia.terza_dose.cumsum()
+#data_Italia['mono_dose_tot'] = data_Italia.mono_dose.cumsum()
+#data_Italia['pregressa_infezione_tot'] = data_Italia.pregressa_infezione.cumsum()
+#if np.argmax(data_Italia.prima_dose_tot > pops['ITA']-data_Italia.iloc[:,-2] - data_Italia.iloc[:,-1]):
+#    max_idx = np.argmax(data_Italia.prima_dose_tot > pops['ITA']-data_Italia.iloc[:,-2] - data_Italia.iloc[:,-1])
+#    data_Italia.iloc[max_idx:, 0] = 0
+#    data_Italia.iloc[max_idx:, 3] = 0
+#    data_Italia.iloc[max_idx:, 4] = 0
+#    data_Italia.iloc[max_idx, 0] = pops['ITA'] - data_Italia.iloc[max_idx-1,-1] -data_Italia.iloc[max_idx-1,-2] - data_Italia.iloc[max_idx-1,5]
+#data_Italia['prima_dose_tot'] = data_Italia.prima_dose.cumsum()
+#data_Italia['seconda_dose_tot'] = data_Italia.seconda_dose.cumsum()
+#data_Italia['mono_dose_tot'] = data_Italia.mono_dose.cumsum()
+#data_Italia['pregressa_infezione_tot'] = data_Italia.pregressa_infezione.cumsum()
+#data_Italia.loc[data_Italia.seconda_dose_tot > 0.95*data_Italia.prima_dose_tot, 'seconda_dose'] = data_Italia.loc[data_Italia.seconda_dose_tot > 0.95 * data_Italia.prima_dose_tot, 'prima_dose']
+#data_Italia.loc[data_Italia.seconda_dose_tot > data_Italia.prima_dose_tot, 'seconda_dose'] = data_Italia.loc[data_Italia.seconda_dose_tot > data_Italia.prima_dose_tot, 'prima_dose']
+
+#for n,i in enumerate(data_Italia.index):
+#    if data_Italia.seconda_dose_tot[i] - data_Italia.mono_dose_tot[i] - data_Italia.pregressa_infezione_tot[i] > data_Italia.prima_dose_tot[i]:
+#        data_Italia.iloc[n, 1] = data_Italia.iloc[n-1,5] - data_Italia.iloc[n-1,6] + data_Italia.iloc[n-1,-2] + data_Italia.iloc[n-1,-1]
+#        data_Italia['seconda_dose_tot'] = data_Italia.seconda_dose.cumsum()
+
+
+
+#data_Italia.loc[data_Italia.seconda_dose_tot > data_Italia.prima_dose_tot, 'seconda_dose'] = data_Italia.loc[data_Italia.seconda_dose_tot > data_Italia.prima_dose_tot, 'prima_dose_tot'] - data_Italia.loc[data_Italia.seconda_dose_tot > data_Italia.prima_dose_tot, 'seconda_dose_tot'] 
+#data_Italia.drop(columns=['prima_dose_tot','seconda_dose_tot'],inplace=True)
+
+regions = [(a,x) for a, x in data.groupby(['area'])]
+for a,x in regions:
+    
+    monodose = x[x['fornitore']=='Janssen']
+
+    #x.drop(monodose.index, inplace=True)
+    monodose = monodose.groupby('data_somministrazione').sum()
+    x = x.groupby('data_somministrazione').sum()
+    x['mono_dose'] = monodose['prima_dose']
+    x['terza_dose'] = x.dose_addizionale_booster
+    x.fillna(0,inplace=True)
+    #x.loc[pd.to_datetime('2021-08-01'),['prima_dose','seconda_dose']] = 3e5
+    #if a=='LOM':
+    #    x.loc[pd.to_datetime(today),['terza_dose']] = 7.5e4 
+    x.loc[max_Italia_index + pd.Timedelta(1, 'day')] = x.iloc[-7:,:].mean().round()
+    data_reg = x.reindex(new_index, columns=['prima_dose', 'seconda_dose', 'terza_dose', 'pregressa_infezione', 'mono_dose']).ffill()
+    data_reg['prima_dose_tot'] = data_reg.prima_dose.cumsum()
+    data_reg['seconda_dose_tot'] = data_reg.seconda_dose.cumsum()
+    data_reg.loc[data_reg.prima_dose_tot > pops[a], 'prima_dose'] = 0
+    data_reg['prima_dose_tot'] = data_reg.prima_dose.cumsum()
+    data_reg.loc[data_reg.seconda_dose_tot > data_reg.prima_dose_tot, 'seconda_dose'] = 0
+    data_reg.loc[data_reg.seconda_dose_tot > data_reg.prima_dose_tot, 'prima_dose'] 
+    #data_reg.drop(columns=['prima_dose_tot','seconda_dose_tot'],inplace=True)
+
+    data_reg.to_csv('data/vaccini_regioni/' + sigla_regioni[a] + '_age.csv', index_label=['data','eta'])
+
+data_Italia.to_csv('data/vaccines_age.csv', index_label=['data','eta'])
+data_Italia.to_csv('data/vaccini_regioni/Italia_age.csv', index_label=['data','eta'])
